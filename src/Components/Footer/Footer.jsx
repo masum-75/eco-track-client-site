b288e5e6d02556dcb41889504b621d1337ac1f20
masum-75 <tanbirmasum014@gmail.com>
2025-11-15 22:05:35 +0600

    navbar and fotter impeliment

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,157 @@
+import React from "react";
+import { Link } from "react-router";
+import { FaFacebookF, FaInstagram, FaLinkedinIn, FaYoutube } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+
+const Footer = () => {
+
+
+
+    return (
+        <footer className=" bg-[#1c2c44] text-white py-10">
+            <div className="container mx-auto px-6 grid grid-cols-1 md:grid-cols-4 gap-8">
+                {/* Brand Section */}
+                <div>
+                    <div className="flex items-center mb-3">
+                        <Link to={"/"} className=" flex  items-center text-[#82B532] text-xl font-semibold"><figure className='w-12 pr-1'><img src={"https://i.ibb.co.com/tpnX8gT8/site-logo2.png"} alt="Site Logo" /></figure><span className='text-[#297B33]'>Eco</span>Track</Link>
+                    </div>
+                    <p className="text-sm leading-relaxed text-gray-100">
+                       Inspiring eco-minded people to live sustainably, embrace green challenges, and create real impact in their communities.
+                    </p>
+                </div>
+
+                {/* Quick Links */}
+                <div>
+                    <h3 className="text-lg font-semibold text-[#82B532] mb-3">
+                        Quick Links
+                    </h3>
+                    <ul className="space-y-2">
+                        <li>
+                            <Link to="/" className="hover:text-[#82B532]">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/challenges" className="hover:text-[#82B532]">
+                                Challenges
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link to="/contact" className="hover:text-[#82B532]">
+                                Contact
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/about" className="hover:text-[#82B532]">
+                                About Us
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+
+                {/* Resources */}
+                <div>
+                    <h3 className="text-lg font-semibold text-[#82B532] mb-3">
+                        Resources
+                    </h3>
+                    <ul className="space-y-2">
+                        <li>
+                            <Link to="/eco-tips" className="hover:text-[#82B532]">
+                                Eco Tips
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/events" className="hover:text-[#82B532]">
+                                Events
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/impact-tracker" className="hover:text-[#82B532]">
+                                Impact Tracker
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/community" className="hover:text-[#82B532]">
+                                Community
+                            </Link>
+                        </li>
+
+                    </ul>
+                </div>
+
+                {/* Newsletter */}
+                <div>
+                    <h3 className="text-lg font-semibold text-[#82B532] mb-3">
+                        Follow Us
+                    </h3>
+                    {/* Social Media */}
+                    <div className="flex flex-col my-4">
+                        <div className="flex space-x-4 text-xl">
+                            <Link
+                                to={"/"}
+                                target="_blank"
+                                className="hover:text-[#82B532] transition-colors"
+                                aria-label="Facebook"
+                            >
+                                <FaFacebookF />
+                            </Link>
+                            <Link
+                                to={"/"}
+                                target="_blank"
+                                className="hover:text-[#82B532] transition-colors"
+                                aria-label="Twitter"
+                            >
+                                <FaXTwitter />
+                            </Link>
+                            <Link
+                                to={"/"}
+                                target="_blank"
+                                className="hover:text-[#82B532] transition-colors"
+                                aria-label="Instagram"
+                            >
+                                <FaInstagram />
+                            </Link>
+                            <Link
+                                to={"/"}
+                                target="_blank"
+                                className="hover:text-[#82B532] transition-colors"
+                                aria-label="LinkedIn"
+                            >
+                                <FaLinkedinIn />
+                            </Link>
+                            <Link
+                                to={"/"}
+                                target="_blank"
+                                className="hover:text-[#82B532] transition-colors"
+                                aria-label="YouTube"
+                            >
+                                <FaYoutube />
+                            </Link>
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
+            <div className=" mt-10 border-t border-[#82B532]/30 pt-5 text-center text-sm text-gray-200">
+                <p>
+                    © {new Date().getFullYear()} <span className="text-[#82B532]">EcoTrack</span> —
+                    Building a sustainable future together.
+                </p>
+                <p>
+                    Developed by —
+                    <Link to={"https://github.com/masum-75"} className="text-[#82B532]">Tanbir</Link>
+                </p>
+
+
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,158 @@
+import React from "react";
+import { useContext } from "react";
+import { Link, NavLink } from "react-router";
+import { AuthContext } from "../../Context/AuthContext";
+import Container from "../../Layouts/Container";
+
+const Navbar = () => {
+  const { user, signOutUser, loading } = useContext(AuthContext);
+
+  const links = (
+    <>
+      <li>
+        <NavLink to={"/"} className={"font-semebold"}>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to={"/challenges"} className={"font-semebold"}>
+          Challenges
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to={"/eco-tips"} className={"font-semebold"}>
+          Eco Tips
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to={"/events"} className={"font-semebold"}>
+          Events
+        </NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="shadow-sm bg-white/30 backdrop-blur  sticky top-0 z-10">
+      <Container>
+        <nav>
+          <div className="navbar ">
+            <div className="navbar-start">
+              <div className="dropdown">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost lg:hidden"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    {" "}
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h8m-8 6h16"
+                    />{" "}
+                  </svg>
+                </div>
+                <ul
+                  tabIndex="-1"
+                  className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+                >
+                  {links}
+                </ul>
+              </div>
+              <Link
+                to={"/"}
+                className=" flex  items-center text-[#82B532] text-xl font-semibold"
+              >
+                <figure className="w-12 pr-1">
+                  <img
+                    src={"https://i.ibb.co.com/tpnX8gT8/site-logo2.png"}
+                    alt="Site Logo"
+                  />
+                </figure>
+                <span className="text-[#297B33]">Eco</span>Track
+              </Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+              <ul className="menu menu-horizontal px-1">{links}</ul>
+            </div>
+            <div className="navbar-end">
+              {loading ? (
+                <span className="loading loading-ring loading-xl"></span>
+              ) : user ? (
+                <div className="dropdown dropdown-end">
+                  <div tabIndex={0} role="button" className=" m-1">
+                    <img
+                      className="w-10 mx-auto rounded-full overflow-hidden border border-primary"
+                      src={
+                        user?.photoURL ||
+                        "https://i.ibb.co.com/tp3xgXbG/avater.jpg"
+                      }
+                      alt="Avater"
+                    />
+                  </div>
+                  <ul
+                    tabIndex="-1"
+                    className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm space-y-3 text-center"
+                  >
+                    <Link
+                      to={"/profile"}
+                      className="tooltip"
+                      data-tip="Click to Profile"
+                    >
+                      <img
+                        className="w-25 mx-auto rounded-full overflow-hidden border border-primary"
+                        src={
+                          user?.photoURL ||
+                          "https://sanishtech.com/i/6918a48ec775c7.75838870-1763222670.png"
+                        }
+                        alt="Avater"
+                      />
+                    </Link>
+                    <NavLink
+                      to={`/challenges-add`}
+                      className={" text-center font-semebold hover:underline"}
+                    >
+                      Add Challenges
+                    </NavLink>
+                    <NavLink to="/my-activities" className=" hover:underline">
+                      My Activities
+                    </NavLink>
+
+                    <h2 className="text-xl font-semebold">
+                      {user?.displayName}
+                    </h2>
+                    <p className="text-black">{user?.email}</p>
+                    <button
+                      onClick={signOutUser}
+                      className={
+                        "btn bg-[#297B33] hover:bg-[#82B532]  text-white"
+                      }
+                    >
+                      Sign Out
+                    </button>
+                  </ul>
+                </div>
+              ) : (
+                <Link
+                  to="/login"
+                  className="btn text-white bg-[#297B33] hover:bg-[#82B532] border-none"
+                >
+                  Log in
+                </Link>
+              )}
+            </div>
+          </div>
+        </nav>
+      </Container>
+    </div>
+  );
+};
+
+export default Navbar;
