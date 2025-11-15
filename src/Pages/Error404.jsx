3cb3b6aeeeef354f55a48cc64d7e43669f809a86
masum-75 <tanbirmasum014@gmail.com>
2025-11-15 23:50:15 +0600

    error page and forgot pass page add

---
+++ src/Pages/Error404.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useNavigate } from "react-router";
+import { AlertCircle, XOctagon } from "lucide-react";
+
+const Error404 = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-50 text-center px-4">
+      <XOctagon
+        className="w-24 h-24 text-[#297B33] animate-bounce mb-6"
+      />
+      <h1 className="text-8xl font-extrabold text-[#297B33] mb-4 animate-pulse">
+        404
+      </h1>
+      <h2 className="text-2xl md:text-3xl font-semibold mb-2">
+        Page Not Found
+      </h2>
+      <p className="text-gray-600 mb-6">
+        The page you are looking for does not exist or has been moved.
+      </p>
+      <button
+        onClick={() => navigate("/")}
+        className="btn bg-[#297B33] hover:bg-[#82B532] text-white px-6 py-3 rounded-xl transition-all flex items-center justify-center gap-2"
+      >
+        <AlertCircle className="w-5 h-5" /> Go Home
+      </button>
+    </div>
+  );
+};
+
+export default Error404;
---
+++ src/Pages/Events.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { FaCalendarCheck } from "react-icons/fa";
+import useEvents from "../Hooks/useEvents";
+import EventsCard from "../Components/EventsCard";
+import EcoTipCardSkeleton from "../Components/EcoTipCardSkeleton";
+import Container from "../Layouts/Container";
+
+const Events = () => {
+  const { events, loading, error } = useEvents();
+  
+
+  if (loading) {
+    return (
+      <Container>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-10">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((n) => (
+            <EcoTipCardSkeleton key={n} />
+          ))}
+        </div>
+      </Container>
+    );
+  }
+
+  if (error) {
+    return <p className="text-red-500 text-center mt-10">{error}</p>;
+  }
+
+  return (
+    <section className="py-16 bg-base-200" id="upcoming-events">
+      <div className="container mx-auto px-5">
+      
+        <div className="text-center mb-10">
+          <div className="flex justify-center text-[#297B33] items-center gap-2 mb-2">
+            <FaCalendarCheck size={24} />
+            <h2 className="text-3xl font-bold">All Events</h2>
+          </div>
+          <p className="text-gray-600 max-w-xl mx-auto">
+            Join our community initiatives and make a real difference in the
+            environment.
+          </p>
+        </div>
+
+        <div className="grid justify-center sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          {events.map((event) => (
+            <EventsCard key={event._id} event={event} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Events;
